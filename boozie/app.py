07ace01377bd6955322bb6661a5ddcc20fd84fb9
Heinz Eckert <heinz.eckert@magenta.at>
2023-11-21 17:07:00 +0100

    model selection and table

--- boozie/app.py
+++ boozie/app.py
@@ -25,7 +25,8 @@ def main():
     # Allow the user to select up to 5 features
     features = st.multiselect("Select features", list(df.columns[:-1]), default=list(df.columns)[:5])
 
-    if len(features) > 5:
+    # TODO: Put in cofig
+    if len(features) > 12:
         st.error("Please select no more than 5 features.")
         return
 
@@ -40,6 +41,20 @@ def main():
     # Reset button
     if st.button("Reset"):
         reset_results()
+
+    # Selectbox for choosing a single model
+    model_choices = [f'Run {i+1}' for i in range(len(st.session_state['results']))]
+    selected_model = st.selectbox("Select a Model for Prediction", model_choices)
+
+    # Load the separate dataset
+    prediction_df = pd.read_csv(path / "data" / "samples.csv")
+
+    # Button to make predictions
+    if st.button("Make Predictions"):
+        make_predictions(selected_model, prediction_df)
+
+    # TODO
+    # add rank, result summary, coloring
 
 
 @dataclass
@@ -65,7 +80,7 @@ def update_plot():
     plt.figure(figsize=(10, 6))
 
     plt.xlabel('Model Run')
-    plt.ylabel('Mean Squared Error')
+    plt.ylabel('Mean Squared Error\n(lower is better)')
     plt.title('Model Performance Over Runs')
 
     if len(df) > 0:
@@ -77,6 +92,34 @@ def update_plot():
     # Display the plot in the Streamlit app
     st.pyplot(plt)
 
+
+def make_predictions(selected_model, prediction_df):
+    if not selected_model:
+        st.error("Please select a model.")
+        return
+
+    run_number = int(selected_model.split(' ')[-1]) - 1
+    training_result = st.session_state['results'][run_number]
+    model = training_result.model
+    features = training_result.features
+
+    # Generate predictions
+    predictions = model.predict(prediction_df[features])
+
+    # Add predictions to the DataFrame and sort
+    prediction_df['predictions'] = predictions
+
+    # TODO
+    # Move to seperate function refine_display_df
+    prediction_df = prediction_df[["name", "predictions", "quality"] + features]
+    sorted_df = (prediction_df.sort_values(by='predictions', ascending=False)
+                              .reset_index(drop=True)
+                              .rename(columns={"quality": "actual quality score"}))
+
+    # Display the sorted DataFrame
+    st.write("Predictions Ordered by Model:", sorted_df)
+
+                              
 
 def reset_results():
     st.session_state['results'] = []
