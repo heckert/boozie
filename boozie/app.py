d88c5e71e0959bb106417f05ec87801a2b4c2ecc
Heinz Eckert <heinz.eckert@magenta.at>
2023-11-23 17:55:26 +0100

    updated to star scores

--- boozie/app.py
+++ boozie/app.py
@@ -1,10 +1,13 @@
 from dataclasses import dataclass
 from pathlib import Path
 
+from typing import Optional
+
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from omegaconf import OmegaConf
 from sklearn.pipeline import Pipeline
 
 from ml.model import ModelTrainer
@@ -19,20 +22,24 @@ if 'results' not in st.session_state:
 
 # Streamlit app
 def main():
+
+    # Parse config
+    file_path = Path(__file__).parent
+    cfg = OmegaConf.load(file_path / "conf" / "config.yaml")
+
     st.title("Wine Quality Predictor")
 
     # Allow the user to select up to 5 features
     features = st.multiselect("Select features", list(df.columns[:-1]), default=list(df.columns)[:5])
 
-    # TODO: Put in cofig
-    if len(features) > 12:
-        st.error("Please select no more than 5 features.")
+    if len(features) > cfg.max_features:
+        st.error(f"Please select no more than {cfg.max_features} features.")
         return
 
     # Button to train the model
     if st.button("Train Model"):
         # Train and evaluate the model
-        train_and_evaluate_model(features)
+        train_and_evaluate_model(features, target=cfg.target)
 
     # Display results
     update_plot()
@@ -43,17 +50,34 @@ def main():
 
     # Selectbox for choosing a single model
     model_choices = [f'Run {i+1}' for i in range(len(st.session_state['results']))]
-    selected_model = st.selectbox("Select a Model for Prediction", model_choices)
-
-    # Load the separate dataset
-    prediction_df = pd.read_csv(path / "data" / "samples.csv")
+    selected_model: str = st.selectbox("Select a Model for Prediction", model_choices)
+    
+    # Load the samples dataset
+    samples = (pd.read_csv(path / "data" / "samples.csv")
+                 .sort_values(cfg.target, ascending=False)
+    )
 
     # Button to make predictions
     if st.button("Make Predictions"):
-        make_predictions(selected_model, prediction_df)
+        predictions = get_predictions(selected_model, samples)
+        # TODO
+        # * add predictions do dataframe
+        # color columns 
+        # add metrics
+        # https://docs.streamlit.io/library/api-reference/data/st.metric
+        
 
-    # TODO
-    # add rank, result summary, coloring
+    st.dataframe(
+        samples[["name", cfg.target]], 
+        hide_index=True,
+        column_config={
+            "name": "Brand",
+            cfg.target: st.column_config.NumberColumn(
+                "Expert Rating",
+                format="%d ⭐"
+            )
+        }
+    )
 
 
 @dataclass
@@ -63,8 +87,8 @@ class TrainingResult:
     features: list
 
 
-def train_and_evaluate_model(features):
-    trainer = ModelTrainer(df[features], df["quality"])
+def train_and_evaluate_model(features: list, *, target: str):
+    trainer = ModelTrainer(df[features], df[target])
     trainer.train()
 
     result = TrainingResult(trainer.evaluate(), trainer.model, features)
@@ -92,33 +116,21 @@ def update_plot():
     st.pyplot(plt)
 
 
-def make_predictions(selected_model, prediction_df):
+def get_predictions(selected_model: str, samples: pd.DataFrame) -> pd.Series:
     if not selected_model:
         st.error("Please select a model.")
         return
 
     run_number = int(selected_model.split(' ')[-1]) - 1
+
     training_result = st.session_state['results'][run_number]
     model = training_result.model
     features = training_result.features
 
-    # Generate predictions
-    predictions = model.predict(prediction_df[features])
+    return pd.Series(
+        model.predict(samples[features])
+    )
 
-    # Add predictions to the DataFrame and sort
-    prediction_df['predictions'] = predictions
-
-    # TODO
-    # Move to seperate function refine_display_df
-    prediction_df = prediction_df[["name", "predictions", "quality"] + features]
-    sorted_df = (prediction_df.sort_values(by='predictions', ascending=False)
-                              .reset_index(drop=True)
-                              .rename(columns={"quality": "actual quality score"}))
-
-    # Display the sorted DataFrame
-    st.write("Predictions Ordered by Model:", sorted_df)
-
-                              
 
 def reset_results():
     st.session_state['results'] = []
--- boozie/get_data.py
+++ boozie/get_data.py
@@ -10,7 +10,7 @@ import pandas as pd
 from omegaconf import OmegaConf
 
 
-def load_wine() -> pd.DataFrame:
+def load_wine(transform_quality_to_stars: bool = True) -> pd.DataFrame:
 
     url = "https://archive.ics.uci.edu/static/public/186/wine+quality.zip"
 
@@ -25,14 +25,18 @@ def load_wine() -> pd.DataFrame:
             zip_file.extractall(tmpdirname)
             zip_file.close()
 
-            red = pd.read_csv(
-                Path(tmpdirname) / "winequality-red.csv", sep=";"
-            )
+            #red = pd.read_csv(
+            #    Path(tmpdirname) / "winequality-red.csv", sep=";"
+            #)
             white = pd.read_csv(
                 Path(tmpdirname) / "winequality-white.csv", sep=";"
             )
 
-        df = pd.concat([red, white], ignore_index=True)
+        df = pd.concat([white], ignore_index=True)
+        if transform_quality_to_stars:
+            df = (df.assign(stars=lambda df: transform_to_star_score(df["quality"]))
+                    .drop("quality", axis=1)
+            )
 
     else:
         response.raise_for_status()
@@ -40,15 +44,26 @@ def load_wine() -> pd.DataFrame:
     return df
 
 
+def transform_to_star_score(scores: pd.Series) -> pd.Series:
+    result = pd.Series(index=scores.index)
+    result[scores.isin(range(5))] = 1
+    result[scores == 5] = 2
+    result[scores == 6] = 3
+    result[scores == 7] = 4
+    result[scores > 7] = 5
+
+    return result
+
+
 def extract_samples(
-    df: pd.DataFrame, samples: dict, random_state: int = 42
+    df: pd.DataFrame,*, score_name: str, samples: dict, random_state: int = 42
 ) -> Tuple[pd.DataFrame, pd.DataFrame]:
     
     df = df.copy()
     samples_df = pd.DataFrame()
 
     for name, score in samples.items():
-        subset = df[df["quality"] == score]
+        subset = df[df[score_name] == score]
 
         record = (subset.sample(random_state=random_state)
                         .assign(name=name))
@@ -58,17 +73,21 @@ def extract_samples(
     return samples_df, df
 
 
-if __name__ == "__main__":
-
+def main() -> None:
     file_path = Path(__file__).parent
 
     cfg = OmegaConf.load(file_path / "conf" / "config.yaml")
 
     df = load_wine()
     samples, df = extract_samples(df,
+                                  score_name=cfg.target,
                                   samples=cfg.samples, 
                                   random_state=cfg.random_state)
 
     output_path = file_path / "data"
     df.to_csv(output_path / "ml.csv", index=False, sep=",")
     samples.to_csv(output_path / "samples.csv", index=False, sep=",")
+
+
+if __name__ == "__main__":
+    main()
